33b7bec9a3f1b7d3ad79e93768e8e7b2bd4d806e
Jaabir Adeniji-Bello <tolubori07@gmail.com>
2024-12-25 16:49:33 +0000

    [any message]

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,17 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("$ ", .{});
-
-    const stdin = std.io.getStdIn().reader();
-    var buffer: [1024]u8 = undefined;
-    const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
-
-    //********REPL*********//
-    std.debug.print("$ ", .{});
     while (true) {
+        const stdout = std.io.getStdOut().writer();
+        try stdout.print("$ ", .{});
+
+        const stdin = std.io.getStdIn().reader();
+        var buffer: [1024]u8 = undefined;
+        const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
+
+        //********REPL*********//
+        std.debug.print("$ ", .{});
+
         try stdout.print("{s}: command not found\n", .{user_input});
     }
 }

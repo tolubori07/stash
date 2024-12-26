4ce34b028ec5aa2b80e43ac1bb57a4494bf632c5
Jaabir Adeniji-Bello <tolubori07@gmail.com>
2024-12-26 02:05:37 +0000

    [any message]

--- src/main.zig
+++ src/main.zig
@@ -8,6 +8,15 @@ pub fn main() !void {
         const stdin = std.io.getStdIn().reader();
         var buffer: [1024]u8 = undefined;
         const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
-        try stdout.print("{s}: command not found\n", .{user_input});
+        var iter = std.mem.splitScalar(u8, user_input, ' ');
+        const command = iter.next();
+        if (command) |c| {
+            if (std.mem.eql(u8, c, "exit")) {
+                const exit_code = try std.fmt.parseInt(u8, iter.next() orelse "0", 10);
+                std.process.exit(exit_code);
+            } else {
+                try stdout.print("{s}: command not found\n", .{user_input});
+            }
+        }
     }
 }

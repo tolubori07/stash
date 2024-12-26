279698b637a4d955f53905464be507268a19cf35
Jaabir Adeniji-Bello <tolubori07@gmail.com>
2024-12-26 02:32:22 +0000

    [any message]

--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,31 @@
 const std = @import("std");
 
+const io = std.io;
+
+const mem = std.mem;
+
+const process = std.process;
+const fmt = std.fmt;
+
 pub fn main() !void {
     while (true) {
-        const stdout = std.io.getStdOut().writer();
+        const stdout = io.getStdOut().writer();
         try stdout.print("$ ", .{});
 
-        const stdin = std.io.getStdIn().reader();
+        const stdin = io.getStdIn().reader();
         var buffer: [1024]u8 = undefined;
         const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
-        var iter = std.mem.splitScalar(u8, user_input, ' ');
-        const command = iter.next();
-        if (command) |c| {
-            if (std.mem.eql(u8, c, "exit")) {
-                const exit_code = try std.fmt.parseInt(u8, iter.next() orelse "0", 10);
-                std.process.exit(exit_code);
-            } else {
-                try stdout.print("{s}: command not found\n", .{user_input});
-            }
+        var tokens = mem.splitScalar(u8, user_input, ' ');
+        const command = tokens.first();
+        const args = tokens.rest();
+        if (mem.eql(u8, command, "exit")) {
+            //example '5'(ASCII value of 53) - '0'(ASCII value of 48) = 5
+            process.exit(args[0] - '0');
+        } else if (std.mem.eql(u8, command, "echo")) {
+            _ = try stdout.write(args);
+            _ = try stdout.write("\n");
+        } else {
+            try stdout.print("{s}: command not found\n", .{user_input});
         }
     }
 }

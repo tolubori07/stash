f98ac819c773ca1c8b252f50639439f4962d8d77
Jaabir Adeniji-Bello <tolubori07@gmail.com>
2024-12-26 03:23:36 +0000

    [any message]

--- src/main.zig
+++ src/main.zig
@@ -8,6 +8,7 @@ const process = std.process;
 const fmt = std.fmt;
 
 pub fn main() !void {
+    const builtins = [_][]const u8{ "echo", "exit", "type" };
     while (true) {
         const stdout = io.getStdOut().writer();
         try stdout.print("$ ", .{});
@@ -20,10 +21,25 @@ pub fn main() !void {
         const args = tokens.rest();
         if (mem.eql(u8, command, "exit")) {
             //example '5'(ASCII value of 53) - '0'(ASCII value of 48) = 5
-            process.exit(args[0] - '0');
+            if (args.len > 0) {
+                process.exit(args[0] - '0');
+            } else {
+                process.exit(0);
+            }
         } else if (std.mem.eql(u8, command, "echo")) {
             _ = try stdout.write(args);
             _ = try stdout.write("\n");
+        } else if (std.mem.eql(u8, command, "type")) {
+            var found = false;
+            for (builtins) |value| {
+                if (mem.eql(u8, args, value)) {
+                    try stdout.print("{s}: is a shell builtin\n", .{args});
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+                try stdout.print("{s}: command not found\n", .{args});
         } else {
             try stdout.print("{s}: command not found\n", .{user_input});
         }
